5bc3349b01b7b120c0b28185b17d93614523d00e
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2024-06-07 22:30:45 -0300

    feat(image): consumes CAT API

--- backend/src/infra/router.ts
+++ backend/src/infra/router.ts
@@ -1,7 +1,18 @@
 import express, { Request, Response } from 'express';
+import ImageService from '../service/image.service';
 
 export const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
-    // TODO implement
+router.get('/images', async (req: Request, res: Response) => {
+    try {
+        
+        const imageService = new ImageService();
+        const imageListOutput = await imageService.list();
+        
+        
+        res.json(imageListOutput);
+    } catch (error) {
+        
+        res.status(500).json({ message: 'Erro ao buscar imagens' });
+    }
 })
--- backend/src/service/image.service.ts
+++ backend/src/service/image.service.ts
@@ -1,9 +1,32 @@
-import { ImageListOutput } from "../dto/image.dto";
+import axios from 'axios';
+import { ImageListOutput, ImageOutput } from '../dto/image.dto';
 
 export default class ImageService {
 
     async list(): Promise<ImageListOutput> {
-        return {}
+        try {
+            // Faz uma requisição GET para a API "The Cat API" para obter 5 imagens de gatos
+            const response = await axios.get('https://api.thecatapi.com/v1/images/search', {
+                params: { limit: 5 },
+                headers: {
+                    'x-api-key': process.env.CAT_API_KEY, // Adiciona a chave da API no header
+                },
+            });
+
+            
+            const images: ImageOutput[] = response.data.map((image: any) => ({
+                id: image.id,
+                url: image.url,
+                width: image.width,
+                height: image.height,
+            }));
+
+            
+            return { images };
+        } catch (error) {
+            
+            throw new Error('Erro ao buscar imagens da API');
+        }
     }
 
 }
